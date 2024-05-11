ab25ce313acd06667bfc6e00324770eeb134eb7d
Daniel Smith <memorysight@gmail.com>
2024-05-11 15:02:32 -0400

    added colliders so they stay on plane

--- src/ComplexRigidBody.jsx
+++ src/ComplexRigidBody.jsx
@@ -1,27 +1,19 @@
-import { Canvas } from "@react-three/fiber";
-import { Torus, Box, Sphere } from "@react-three/drei";
-import { Suspense } from "react";
+import { Sphere } from "@react-three/drei";
 import {
   Physics,
   RigidBody,
-  CuboidCollider,
-  BallCollider,
-  MeshCollider,
+  BallCollider, CuboidCollider
 } from "@react-three/rapier";
 
 function ComplexRigidBody() {
   return (
     <Physics debug>
-    <RigidBody position={[0, 10, 0]} colliders="ball">
-      <MeshCollider position={[0, -2, 0]} args={[20, 0.5, 20]} type="trimesh"></MeshCollider>
-
-      <MeshCollider type="hull" position={[0, -2, 0]} args={[20, 0.5, 20]}></MeshCollider>
-
+    <RigidBody position={[0, 10, 0]}>
       <Sphere />
-
       <BallCollider args={[0.5]} />
       <BallCollider args={[0.5]} position={[1, 0, 0]} />
-    </RigidBody>
+    </RigidBody> 
+    <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
     </Physics>
   );
 }
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -12,38 +12,27 @@ import { OrbitControls } from "@react-three/drei";
 
 const Scene = () => (
   <Physics debug>
+
+    {/* ///box */}
     <RigidBody>
       <Box />
     </RigidBody>
-    {/* //puts a box around sphere */}
+
+
+
+    {/* ///Sphere with Box */}
     <RigidBody position={[0, 10, 0]}>
       <Sphere />
     </RigidBody>
-    {/* //causes items to hit ground and stay */}
     <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
-
+ 
+{/* 
+     ////Complex Spheres with Box */}
     <RigidBody position={[0, 10, 0]}>
       <Sphere />
       <BallCollider args={[0.5]} />
       <BallCollider args={[0.5]} position={[1, 0, 0]} />
-    </RigidBody>
-
-{/* 
-    <RigidBody position={[0, 10, 0]} colliders='ball'>
-    <MeshCollider type="trimesh">
-      
-    </MeshCollider>
-
-    <MeshCollider type="hull">
-     
-    </MeshCollider>
-
-    <Sphere />
-
-    <BallCollider args={[0.5]} />
-    <BallCollider args={[0.5]} position={[1, 0, 0]} />
-  </RigidBody> */}
-
+    </RigidBody> 
 
 
 
